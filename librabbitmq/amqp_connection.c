31d8ad0e3531a0c9f9d62812157250b94cc7ceaf
Duncan Burke <duncan.burke@orionvm.com.au>
2012-08-29 18:53:24 +1000

    initialise outbound pool
    Signed-off-by: Duncan Burke <duncan.burke@orionvm.com.au>

--- librabbitmq/amqp_connection.c
+++ librabbitmq/amqp_connection.c
@@ -63,6 +63,7 @@ amqp_connection_state_t amqp_new_connection(void) {
 
   init_amqp_pool(&state->frame_pool, INITIAL_FRAME_POOL_PAGE_SIZE);
   init_amqp_pool(&state->decoding_pool, INITIAL_DECODING_POOL_PAGE_SIZE);
+  init_amqp_pool(&state->outbound_pool, INITIAL_DECODING_POOL_PAGE_SIZE);
 
   if (amqp_tune_connection(state, 0, INITIAL_FRAME_POOL_PAGE_SIZE, 0) != 0)
     goto out_nomem;
